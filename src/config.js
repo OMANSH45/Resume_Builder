c98fff629b785c5445a5fe272c361856a750d748
omansh <omanshdhawan45@gmail.com>
2022-07-18 19:24:57 +0530

    done

--- src/config.js
+++ src/config.js
@@ -3,12 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 // Initialize Firebase
 let fbObj = {
-    apiKey: "AIzaSyAmzELW9wLnRhnHb07pcC_lwLIG-V-TPWs",
-    authDomain: "resume-builder-8b5c5.firebaseapp.com",
-    projectId: "resume-builder-8b5c5",
-    storageBucket: "resume-builder-8b5c5.appspot.com",
-    messagingSenderId: "387539545907",
-    appId: "1:387539545907:web:77c16007e1e45b15fe9846"
+    apiKey: "AIzaSyCttf7oNLMMsox5MI4i8F3RKcJymoCA3Vw",
+    authDomain: "resumebuilder-7e879.firebaseapp.com",
+    projectId: "resumebuilder-7e879",
+    storageBucket: "resumebuilder-7e879.appspot.com",
+    messagingSenderId: "1050838208778",
+    appId: "1:1050838208778:web:f35ba5fe5136024dfa2af3",
+    measurementId: "G-7185WB2W44"
 }
 firebase.initializeApp(fbObj);
 export default firebase;
